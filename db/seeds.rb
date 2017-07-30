ed02b87b38d0efe799b7dc8106d4880c860629d9
diwu <di.wu@sage.com>
2017-07-30 15:43:36 -0400

    add bits data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,42 @@ Bit.delete_all
 OtherMaterial.delete_all
 Invention.delete_all
 
-Bit.create([{name: "bargraph"},{name: "bend-sensor"},{name: "branch"},{name: "bright-led"}])
+Bit.create([{name: "bargraph"},
+            {name: "bend-sensor"},
+            {name: "branch"},
+            {name: "bright-led"},
+            {name: "button"},
+            {name: "buzzer"},
+            {name: "coin-battery"},
+            {name: "dc-motor"},
+            {name: "dimmer"},
+            {name: "doubleand"},
+            {name: "doubleor"},
+            {name: "fan"},
+            {name: "forkinverter"},
+            {name: "led"},
+            {name: "light-sensor"},
+            {name: "light-trigger"},
+            {name: "light-wire"},
+            {name: "long-led"},
+            {name: "motion-trigger"},
+            {name: "power"},
+            {name: "pressure-sensor"},
+            {name: "pulse"},
+            {name: "rgb-led"},
+            {name: "roller-switch"},
+            {name: "servo-motor"},
+            {name: "slide-dimmer"},
+            {name: "slide-switch"},
+            {name: "sound-trigger"},
+            {name: "temperature-sensor"},
+            {name: "timeout"},
+            {name: "toggle-switch"},
+            {name: "usb-power"},
+            {name: "uv-led"},
+            {name: "vibration-motor"},
+            {name: "wire"}
+           ])
 OtherMaterial.create([{name: "scissors"}, {name: "tape"}, {name: "construction-paper"}])
 inv = Invention.new({title: "inv 1", description: "des inv 1", user_name: "test1", email: "test1@us.com"})
 inv.bits = [Bit.all[1]]
